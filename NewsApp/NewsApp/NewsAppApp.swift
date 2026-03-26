e792b0b308f3312b30c4a10f2edfca35a68d5d89
Ivan Gabrilo <ivan.gabrilo@fer.hr>
2026-03-26 16:57:52 +0100

    prva zadaca

---
+++ NewsApp/NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by Ivan Gabrilo on 24.03.2026..
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
